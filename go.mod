87aace9bb619cb03dfc1a996719dbe392ca048cf
KnownElephant <pkane096@gmail.com>
2023-03-13 11:57:25 -0400

    first commit

---
+++ go.mod
@@ -0,0 +1,11 @@
+module go-consequences-run-script
+
+go 1.16
+
+require (
+	github.com/USACE/go-consequences v0.0.0-20230302143124-d4d13b9362ce
+	github.com/dewberry/gdal v0.3.2 // indirect
+	github.com/leekchan/accounting v1.0.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/shopspring/decimal v1.2.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/USACE/go-consequences/compute"
+	"github.com/USACE/go-consequences/hazardproviders"
+	"github.com/USACE/go-consequences/resultswriters"
+	"github.com/USACE/go-consequences/structureprovider"
+)
+
+func main() {
+	fmt.Println("Start")
+
+	//define flood hazard, provide the file path to your desired flood
+	hazard, e := hazardproviders.Init("./data/flood_here.tif")
+	if e != nil {
+		panic(e)
+	}
+	defer hazard.Close()
+
+	//define structure inventory, provide file path to structure inventory
+	inventory, b := structureprovider.InitSHP("./data/inventory_here.shp")
+	if b != nil {
+		panic(b)
+	}
+
+	//define result file, provide file path where you want results to be saved
+	results, c := resultswriters.InitGpkResultsWriter("./output/result_file.gpkg", "event_results")
+	if c != nil {
+		panic(c)
+	}
+	defer results.Close()
+
+	//run the compute
+	compute.StreamAbstract(hazard, inventory, results)
+	fmt.Println("End")
+}
